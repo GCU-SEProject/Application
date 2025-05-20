c98d319bebb05628ec3b8a65058705a850c9af04
ZJade1234 <gusrhks1012@gmail.com>
2025-05-20 15:15:57 +0900

    feat: video entity

--- app/src/main/java/com/example/setp/network/ApiService.java
+++ app/src/main/java/com/example/setp/network/ApiService.java
@@ -1,6 +1,8 @@
 package com.example.setp.network;
 
 import com.example.setp.game.Game; // Game import
+import com.example.setp.video.Video; // Video import
+
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,5 +14,7 @@ public interface ApiService {
     @GET("/game-service/search/game")// API Gateway EndPoint
     Call<List<Game>> searchGamesByTags(@Query("tag") List<String> tags);
 
-    // ToDo: Other API EndPoint Definition
+    @GET("/video-service/search/video")
+    Call<List<Video>> searchVideosByKeyword(@Query("keyword") List<String> keyword);
+
 }
